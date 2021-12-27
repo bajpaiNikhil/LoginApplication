492b750266bd16288b3b95bb2550bed50f332777
NikhilBajpai <nikhil.cse16@gmail.com>
2021-12-27 20:39:19 +0530

    day 1

---
+++ app/src/main/java/com/example/loginapplication/data/emtities/TableItem.kt
@@ -0,0 +1,28 @@
+package com.example.loginapplication.data.emtities
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "authenticationTable")
+data class TableItem (
+    @ColumnInfo(name = "userName")
+    var name : String,
+
+    @ColumnInfo(name = "userEmail")
+    var email : String,
+
+    @ColumnInfo(name = "phoneNumber")
+    var phNumber : String ,
+
+    @ColumnInfo(name = "userAddress")
+    var address : String ,
+
+    @ColumnInfo(name  = "userPassword")
+    var password : String
+    ){
+
+    @PrimaryKey(autoGenerate = true)
+    var id : Int? = null
+}
